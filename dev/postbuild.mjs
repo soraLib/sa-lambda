17945b282be57d56b55f822ad19bfd1bdeb7c2fe
hfutsora <346762712@qq.com>
2022-09-15 17:34:40 +0800

    feat: add copy pkg fn

--- dev/postbuild.mjs
+++ dev/postbuild.mjs
@@ -1,15 +1,30 @@
-import ncpRaw from "ncp";
-import { promisify } from "util";
-const ncp = promisify(ncpRaw);
+import fs from 'fs'
+import ncpRaw from "ncp"
+import { promisify } from "util"
+const ncp = promisify(ncpRaw)
 
-const cwd = process.cwd();
+const cwd = process.cwd()
+const OUTPUT_FOLDER = 'dist'
+const OUTPUT_PATH = `${cwd}/${OUTPUT_FOLDER}`
+const PKG = 'package.json'
+
+const copyPackageJson = async () => {
+  const pkg = fs.readFileSync(PKG)
+  const json = JSON.parse(pkg)
+  const clone = Object.assign({}, json)
+  delete clone.scripts
+  delete clone.files
+  delete clone.devDependencies
+
+  fs.writeFileSync(`${OUTPUT_PATH}/${PKG}`, JSON.stringify(clone, null, 2))
+}
 
 await Promise.all([
-  ncp(`${cwd}/package.json`, `${cwd}/dist/package.json`),
-  ncp(`${cwd}/type`, `${cwd}/dist/cjs`),
-  ncp(`${cwd}/type`, `${cwd}/dist/cjs`),
-  ncp(`${cwd}/type`, `${cwd}/dist/esm`),
-  ncp(`${cwd}/type`, `${cwd}/dist/umd`),
-  ncp(`${cwd}/type`, `${cwd}/dist/es`),
+  ncp(`${cwd}/type`, `${OUTPUT_PATH}/cjs`),
+  ncp(`${cwd}/type`, `${OUTPUT_PATH}/cjs`),
+  ncp(`${cwd}/type`, `${OUTPUT_PATH}/esm`),
+  ncp(`${cwd}/type`, `${OUTPUT_PATH}/umd`),
+  ncp(`${cwd}/type`, `${OUTPUT_PATH}/es`),
+  copyPackageJson()
 ])
 
