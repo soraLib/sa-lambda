4d1166b1b012aa72fede8ea57a10dc7afafdf5fe
hfutsora <346762712@qq.com>
2022-09-16 14:13:19 +0800

    feat: make modules

--- darl.config.mjs
+++ darl.config.mjs
@@ -1,11 +1,11 @@
-import { npm, sub, once, queue } from 'darl'
+import { npm, once, queue, sub } from 'darl'
 
 export const build = once([
   queue(
     npm`tsc`('--', '-p', 'tsconfig.build.json'),
     sub(
-      npm`babel`('--', '--config-file', './babel.config.esm.mjs', 'es', '-d', 'esm', '--out-file-extension', '.mjs'),
-      npm`babel`('--', '--config-file', './babel.config.umd.mjs', 'es', '-d', 'umd'),
+      npm`babel`('--', '--config-file', './babel.config.esm.mjs', 'dist/es', '-d', 'dist/esm', '--out-file-extension', '.mjs'),
+      npm`babel`('--', '--config-file', './babel.config.umd.mjs', 'dist/es', '-d', 'dist/umd'),
     ),
   ),
   npm`tsc`('--', '-p', 'tsconfig.cjs.json'),
--- dev/postbuild.mjs
+++ dev/postbuild.mjs
@@ -1,11 +1,13 @@
 import fs from 'fs'
+import glob from 'glob'
 import ncpRaw from "ncp"
+import path from 'path'
 import { promisify } from "util"
 const ncp = promisify(ncpRaw)
 
 const cwd = process.cwd()
 const OUTPUT_FOLDER = 'dist'
-const OUTPUT_PATH = `${cwd}/${OUTPUT_FOLDER}`
+const OUTPUT_PATH = path.join(cwd, OUTPUT_FOLDER)
 const PKG = 'package.json'
 
 const copyPackageJson = () => {
@@ -16,23 +18,48 @@ const copyPackageJson = () => {
   delete clone.files
   delete clone.devDependencies
 
-  fs.writeFileSync(`${OUTPUT_PATH}/${PKG}`, JSON.stringify(clone, null, 2))
+  fs.writeFileSync(path.join(OUTPUT_PATH, PKG), JSON.stringify(clone, null, 2))
 }
 
 const FILES = ['CHANGELOG.md', 'LICENSE', 'README.md']
 const copyFiles = () => {
   for (const file of FILES) {
-    fs.copyFileSync(file, `${OUTPUT_PATH}/${file}`)
+    fs.copyFileSync(file, path.join(OUTPUT_PATH, file))
   }
 }
 
+const makeModules = async () => {
+  const matches = glob.sync('dist/cjs/*.js')
+  const modules = getModules(matches)
+
+  for(const module of modules) {
+    makeSingleModule(module)
+  }
+}
+
+const getModules = paths => 
+   paths.map(filePath => path.basename(filePath, '.js')).filter(x => x !== 'index')
+
+const makePkgJson = module => ({
+    main: `../cjs/${module}.js`,
+    module: `../esm/${module}.js`,
+    types: `../type/${module}.d.ts`,
+    sideEffects: false
+  })
+
+const makeSingleModule = module => {
+  fs.mkdirSync(path.join(OUTPUT_PATH, module))
+  fs.writeFileSync(path.join(OUTPUT_PATH, module, PKG), JSON.stringify(makePkgJson(module), null, 2))
+}
+
 await Promise.all([
-  ncp(`${cwd}/type`, `${OUTPUT_PATH}/cjs`),
-  ncp(`${cwd}/type`, `${OUTPUT_PATH}/cjs`),
-  ncp(`${cwd}/type`, `${OUTPUT_PATH}/esm`),
-  ncp(`${cwd}/type`, `${OUTPUT_PATH}/umd`),
-  ncp(`${cwd}/type`, `${OUTPUT_PATH}/es`),
+  ncp(path.join(cwd, 'type'), path.join(OUTPUT_PATH, 'type')),
+  ncp(path.join(cwd, 'type'), path.join(OUTPUT_PATH, 'cjs')),
+  ncp(path.join(cwd, 'type'), path.join(OUTPUT_PATH, 'esm')),
+  ncp(path.join(cwd, 'type'), path.join(OUTPUT_PATH, 'umd')),
+  ncp(path.join(cwd, 'type'), path.join(OUTPUT_PATH, 'es')),
   copyPackageJson(),
-  copyFiles()
+  copyFiles(),
 ])
 
+makeModules()
